6f7f1c3e0442edfe4e81a61d559c2710bd1e6f7d
raeed1993 <raeed1993@gmail.com>
2023-05-15 20:49:12 +0300

    - add learning path

--- Modules/ResultPractice/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('resultpractice::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('resultpractice.name') !!}
-    </p>
-@endsection
--- resources/views/student/partials/sidebar.blade.php
+++ resources/views/student/partials/sidebar.blade.php
@@ -11,15 +11,15 @@
             </div>
             <ul class="sidebar-menu">
 
-                <li class="sidebar-menu-item active">
-                    <a class="sidebar-menu-button"
-                       href="index.html">
-                        <span class="material-icons sidebar-menu-icon sidebar-menu-icon--left">home</span>
-                        <span class="sidebar-menu-text">
-                            الرئيسية
-                        </span>
-                    </a>
-                </li>
+{{--                <li class="sidebar-menu-item active">--}}
+{{--                    <a class="sidebar-menu-button"--}}
+{{--                       href="index.html">--}}
+{{--                        <span class="material-icons sidebar-menu-icon sidebar-menu-icon--left">home</span>--}}
+{{--                        <span class="sidebar-menu-text">--}}
+{{--                            الرئيسية--}}
+{{--                        </span>--}}
+{{--                    </a>--}}
+{{--                </li>--}}
                 <li class="sidebar-menu-item">
                     <a class="sidebar-menu-button"
                        href="{{route('student.courses.index')}}">
@@ -40,39 +40,39 @@
                         </span>
                     </a>
                 </li>
-                <li class="sidebar-menu-item">
-                    <a class="sidebar-menu-button"
-                       href="student-dashboard.html">
-                                                    <span
-                                                        class="material-icons sidebar-menu-icon sidebar-menu-icon--left">account_box</span>
-                        <span class="sidebar-menu-text">لوحة التحكم</span>
-                    </a>
-                </li>
-                <li class="sidebar-menu-item">
-                    <a class="sidebar-menu-button"
-                       href="student-my-courses.html">
-                        <span class="material-icons sidebar-menu-icon sidebar-menu-icon--left">search</span>
-                        <span class="sidebar-menu-text">
-                            الدروس
+{{--                <li class="sidebar-menu-item">--}}
+{{--                    <a class="sidebar-menu-button"--}}
+{{--                       href="student-dashboard.html">--}}
+{{--                                                    <span--}}
+{{--                                                        class="material-icons sidebar-menu-icon sidebar-menu-icon--left">account_box</span>--}}
+{{--                        <span class="sidebar-menu-text">لوحة التحكم</span>--}}
+{{--                    </a>--}}
+{{--                </li>--}}
+{{--                <li class="sidebar-menu-item">--}}
+{{--                    <a class="sidebar-menu-button"--}}
+{{--                       href="student-my-courses.html">--}}
+{{--                        <span class="material-icons sidebar-menu-icon sidebar-menu-icon--left">search</span>--}}
+{{--                        <span class="sidebar-menu-text">--}}
+{{--                            الدروس--}}
 
-                        </span>
-                    </a>
-                </li>
-                <li class="sidebar-menu-item">
-                    <a class="sidebar-menu-button"
-                       href="student-paths.html">
-                                                    <span
-                                                        class="material-icons sidebar-menu-icon sidebar-menu-icon--left">timeline</span>
-                        <span class="sidebar-menu-text">مساري التعليمي</span>
-                    </a>
-                </li>
-                <li class="sidebar-menu-item">
-                    <a class="sidebar-menu-button"
-                       href="student-path.html">
-                        <span class="material-icons sidebar-menu-icon sidebar-menu-icon--left">change_history</span>
-                        <span class="sidebar-menu-text">تفاصيل المسار</span>
-                    </a>
-                </li>
+{{--                        </span>--}}
+{{--                    </a>--}}
+{{--                </li>--}}
+{{--                <li class="sidebar-menu-item">--}}
+{{--                    <a class="sidebar-menu-button"--}}
+{{--                       href="student-paths.html">--}}
+{{--                                                    <span--}}
+{{--                                                        class="material-icons sidebar-menu-icon sidebar-menu-icon--left">timeline</span>--}}
+{{--                        <span class="sidebar-menu-text">مساري التعليمي</span>--}}
+{{--                    </a>--}}
+{{--                </li>--}}
+{{--                <li class="sidebar-menu-item">--}}
+{{--                    <a class="sidebar-menu-button"--}}
+{{--                       href="student-path.html">--}}
+{{--                        <span class="material-icons sidebar-menu-icon sidebar-menu-icon--left">change_history</span>--}}
+{{--                        <span class="sidebar-menu-text">تفاصيل المسار</span>--}}
+{{--                    </a>--}}
+{{--                </li>--}}
                 <li class="sidebar-menu-item">
                     <a class="sidebar-menu-button"
                        href="{{route('student.courses.show')}}">
@@ -80,29 +80,29 @@
                         <span class="sidebar-menu-text">عرض الدورة التدريبية</span>
                     </a>
                 </li>
-                <li class="sidebar-menu-item">
-                    <a class="sidebar-menu-button"
-                       href="student-lesson.html">
-                        <span class="material-icons sidebar-menu-icon sidebar-menu-icon--left">panorama_fish_eye</span>
-                        <span class="sidebar-menu-text">عرض الدرس</span>
-                    </a>
-                </li>
-                <li class="sidebar-menu-item">
-                    <a class="sidebar-menu-button"
-                       href="student-take-course.html">
-                        <span class="material-icons sidebar-menu-icon sidebar-menu-icon--left">class</span>
-                        <span class="sidebar-menu-text"> احضر الدورة التدريبية </span>
-                        <span
-                            class="sidebar-menu-badge badge badge-accent badge-notifications ml-auto">PRO</span>
-                    </a>
-                </li>
-                <li class="sidebar-menu-item">
-                    <a class="sidebar-menu-button"
-                       href="student-take-lesson.html">
-                        <span class="material-icons sidebar-menu-icon sidebar-menu-icon--left">import_contacts</span>
-                        <span class="sidebar-menu-text">احضر الدرس</span>
-                    </a>
-                </li>
+{{--                <li class="sidebar-menu-item">--}}
+{{--                    <a class="sidebar-menu-button"--}}
+{{--                       href="student-lesson.html">--}}
+{{--                        <span class="material-icons sidebar-menu-icon sidebar-menu-icon--left">panorama_fish_eye</span>--}}
+{{--                        <span class="sidebar-menu-text">عرض الدرس</span>--}}
+{{--                    </a>--}}
+{{--                </li>--}}
+{{--                <li class="sidebar-menu-item">--}}
+{{--                    <a class="sidebar-menu-button"--}}
+{{--                       href="student-take-course.html">--}}
+{{--                        <span class="material-icons sidebar-menu-icon sidebar-menu-icon--left">class</span>--}}
+{{--                        <span class="sidebar-menu-text"> احضر الدورة التدريبية </span>--}}
+{{--                        <span--}}
+{{--                            class="sidebar-menu-badge badge badge-accent badge-notifications ml-auto">PRO</span>--}}
+{{--                    </a>--}}
+{{--                </li>--}}
+{{--                <li class="sidebar-menu-item">--}}
+{{--                    <a class="sidebar-menu-button"--}}
+{{--                       href="student-take-lesson.html">--}}
+{{--                        <span class="material-icons sidebar-menu-icon sidebar-menu-icon--left">import_contacts</span>--}}
+{{--                        <span class="sidebar-menu-text">احضر الدرس</span>--}}
+{{--                    </a>--}}
+{{--                </li>--}}
                 <li class="sidebar-menu-item">
                     <a class="sidebar-menu-button"
                        href="{{route('student.practice.show',['id'=>\DataSource\Entities\PracticeType\PracticeType::first()->id])}}">
@@ -112,36 +112,36 @@
                         </span>
                     </a>
                 </li>
-                <li class="sidebar-menu-item">
-                    <a class="sidebar-menu-button"
-                       href="student-quiz-results.html">
-                        <span class="material-icons sidebar-menu-icon sidebar-menu-icon--left">poll</span>
-                        <span class="sidebar-menu-text">مذاكراتي</span>
-                    </a>
-                </li>
-                <li class="sidebar-menu-item">
-                    <a class="sidebar-menu-button"
-                       href="student-quiz-result-details.html">
-                                                    <span
-                                                        class="material-icons sidebar-menu-icon sidebar-menu-icon--left">live_help</span>
-                        <span class="sidebar-menu-text">نتائج المذاكرات</span>
-                    </a>
-                </li>
-                <li class="sidebar-menu-item">
-                    <a class="sidebar-menu-button"
-                       href="student-path-assessment.html">
-                        <span class="material-icons sidebar-menu-icon sidebar-menu-icon--left">layers</span>
-                        <span class="sidebar-menu-text">Skill Assessment</span>
-                    </a>
-                </li>
-                <li class="sidebar-menu-item">
-                    <a class="sidebar-menu-button"
-                       href="student-path-assessment-result.html">
-                                        <span
-                                            class="material-icons sidebar-menu-icon sidebar-menu-icon--left">assignment_turned_in</span>
-                        <span class="sidebar-menu-text">Skill Result</span>
-                    </a>
-                </li>
+{{--                <li class="sidebar-menu-item">--}}
+{{--                    <a class="sidebar-menu-button"--}}
+{{--                       href="student-quiz-results.html">--}}
+{{--                        <span class="material-icons sidebar-menu-icon sidebar-menu-icon--left">poll</span>--}}
+{{--                        <span class="sidebar-menu-text">مذاكراتي</span>--}}
+{{--                    </a>--}}
+{{--                </li>--}}
+{{--                <li class="sidebar-menu-item">--}}
+{{--                    <a class="sidebar-menu-button"--}}
+{{--                       href="student-quiz-result-details.html">--}}
+{{--                                                    <span--}}
+{{--                                                        class="material-icons sidebar-menu-icon sidebar-menu-icon--left">live_help</span>--}}
+{{--                        <span class="sidebar-menu-text">نتائج المذاكرات</span>--}}
+{{--                    </a>--}}
+{{--                </li>--}}
+{{--                <li class="sidebar-menu-item">--}}
+{{--                    <a class="sidebar-menu-button"--}}
+{{--                       href="student-path-assessment.html">--}}
+{{--                        <span class="material-icons sidebar-menu-icon sidebar-menu-icon--left">layers</span>--}}
+{{--                        <span class="sidebar-menu-text">Skill Assessment</span>--}}
+{{--                    </a>--}}
+{{--                </li>--}}
+{{--                <li class="sidebar-menu-item">--}}
+{{--                    <a class="sidebar-menu-button"--}}
+{{--                       href="student-path-assessment-result.html">--}}
+{{--                                        <span--}}
+{{--                                            class="material-icons sidebar-menu-icon sidebar-menu-icon--left">assignment_turned_in</span>--}}
+{{--                        <span class="sidebar-menu-text">Skill Result</span>--}}
+{{--                    </a>--}}
+{{--                </li>--}}
 
             </ul>
 
