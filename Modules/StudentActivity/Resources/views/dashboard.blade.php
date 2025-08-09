2752cc7576a575057138d1ce765462ace6647bd9
nazeer_mog <nazeermog7@gmail.com>
2025-08-09 05:33:59 +0300

    mirror

--- Modules/StudentActivity/Resources/views/dashboard.blade.php
+++ Modules/StudentActivity/Resources/views/dashboard.blade.php
@@ -12,9 +12,7 @@
                         <li class="breadcrumb-item"><a href="index.html">Home</a></li>
 
                         <li class="breadcrumb-item active">
-
                             Dashboard
-
                         </li>
 
                     </ol>
